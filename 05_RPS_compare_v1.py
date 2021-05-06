333d0847cdfe6b6c98e463785671fd20122e1456
walkera9033 <>
2021-05-06 02:51:13 +0000

    comp_choice

--- 05_RPS_compare_v1.py
+++ 05_RPS_compare_v1.py
@@ -11,15 +11,22 @@ for item in rps_list:
 
 		result = ""
 
-		# Compare options
+	 # Compare options
 		
 		if comp_choice == user_choice:
 			result = "tie"
 		elif comp_choice == "rock" and user_choice == "paper":
 			result = "win"
-    if comp_choice == "paper" and user_choice == "rock":
-		  result = "loose"
+		if comp_choice == "scissors" and user_choice == "paper:
+			result = "loose"
+		elif comp_choice == "paper" and user_choice == "scissors":
+			result = "win"
 
+		if comp_choice == "paper" and user_choice "rock"
+		result = "loose"
+		elif comp_choice == "rock" and user_choice == "paper"
+		result = "win"
+		
 		print("User: {} vs Computer: {} - {}".format(user_choice, comp_choice, result))
 
-  comp_index += 1
+		comp_index += 1
